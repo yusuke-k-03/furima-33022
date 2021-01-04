ffcc49fcd200ffa77b990f00ae314899530b5eb7
yusuke-k-03 <sinkuku01@gmail.com>
2021-01-04 18:30:07 +0900

    入力時エラーの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        with_options presence: true do
+
+          validates :nickname
+          validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+          validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+          validates :last_name_kana, format: {with: /\A[ァ-ン]/} 
+          validates :first_name_kana, format: {with: /\A[ァ-ン]/} 
+          validates :birth_date
+
+        end
+
+
 end
