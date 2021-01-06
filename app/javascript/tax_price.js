d1a68b8355bd30323f612822bea78a0899184d90
yusuke-k-03 <sinkuku01@gmail.com>
2021-01-06 17:19:43 +0900

    販売手数料実装

---
+++ app/javascript/tax_price.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function(){
+
+  const InputPrice = document.getElementById("item-price")
+  InputPrice.addEventListener('input', function(){
+    const inputValue = InputPrice.value;
+    
+    const addTax = document.getElementById("add-tax-price")
+    addTax.innerHTML = Math.floor( inputValue / 10 )
+   
+    
+    
+    const Profit = document.getElementById("profit")
+    Profit.innerHTML = Math.floor( inputValue * 0.9 )
+ })
+})
