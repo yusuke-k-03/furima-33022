28643b2fb669b9ab6a226522540a1695f34bcecb
yusuke-k-03 <sinkuku01@gmail.com>
2021-01-08 19:12:49 +0900

    コントローラーの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,19 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render:edit
+    end
   end
 
 
